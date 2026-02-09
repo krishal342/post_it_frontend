b80d1938e1282a0bde5e1e436c29063df5959ad7
krishal <krishal342@gmail.com>
2026-02-09 19:21:51 +0545

    debug 2

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,72 +1,42 @@
-import { cookies } from "next/headers";
+
+
+"use client";
+
+import { useEffect, useState } from "react";
 import Card from "@/components/card/card";
 
-export default async function Home() {
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const cookieStore = await cookies();
-  const loginToken = cookieStore.get('loginToken');
+  useEffect(() => {
+    const fetchPosts = async () => {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/home`,
+          {
+            method: "GET",
+            credentials: "include", // ✅ send cookies
+          }
+        );
 
+        const resData = await response.json();
+        setPosts(resData);
+        setLoading(false);
 
-  const respons = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/home`, {
-    method: 'GET',
-    // credentials: 'include',  // don't work
-    // this is server side rendering so cookies are available from browser so need manually add cookies
-    headers: {
-      Cookie: `loginToken=${loginToken?.value}`
-    }
-    // credentials: 'include',
-  });
+    };
 
+    fetchPosts();
+  }, []);
 
-
-  const resData = await respons.json();
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="flex flex-col w-[400px]">
-      {resData.map((post) => (
+      {posts.map((post) => (
         <Card key={post.id} post={post} />
       ))}
     </div>
   );
 }
-
-// "use client";
-
-// import { useEffect, useState } from "react";
-// import Card from "@/components/card/card";
-
-// export default function Home() {
-//   const [posts, setPosts] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchPosts = async () => {
-//         const response = await fetch(
-//           `${process.env.NEXT_PUBLIC_API_URL}/home`,
-//           {
-//             method: "GET",
-//             credentials: "include", // ✅ send cookies
-//           }
-//         );
-
-//         const resData = await response.json();
-//         setPosts(resData);
-//         setLoading(false);
-
-//     };
-
-//     fetchPosts();
-//   }, []);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div className="flex flex-col w-[400px]">
-//       {posts.map((post) => (
-//         <Card key={post.id} post={post} />
-//       ))}
-//     </div>
-//   );
-// }
